c346fd64d41a9d3a89dc6164aa53e61174e6008b
bettergrammer <wiesnerjjason@gmail.com>
2021-09-14 21:35:51 -0700

    Update file structure

---
+++ client/src/renderPhotoService.jsx
@@ -0,0 +1,4 @@
+import PhotoService from './PhotoService.jsx';
+import '../../public/style.css';
+
+ReactDOM.render(<PhotoService/>, document.getElementById('PhotoService'));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/client/src/renderPhotoService.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: '',
+  output: {
+   filename: 'bundle.js',
+   path: __dirname + '/public'
+  },
+  plugins: [
+		new BrotliPlugin({
+			asset: '[path].br[query]',
+			test: /\.(js|css|html|svg)$/,
+			threshold: 10240,
+			minRatio: 0.8
+    }),
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()//Merge chunks
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        }
+      }),
+    ],
+  }
+};
